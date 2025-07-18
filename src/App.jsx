e461a6838ae148eb2c59138d5c3234155dd6d29a
Louise Morais <louisemorais3@gmail.com>
2025-07-18 17:51:05 -0300

    feat: subindo a navbar

--- src/App.jsx
+++ src/App.jsx
@@ -9,6 +9,13 @@ function App() {
           <h1>Energia Solar 365</h1>
           <p>Economize com o sol. Invista no seu futuro!</p>
         </header>
+                  
+          <nav className='navbar'>
+            <a href="#sobreNos">sobre nós</a>
+            <a href="#porqueInvestir">porque investir?</a>
+            <a href="#produto">produtos</a>
+            <a href="#orcamento">orçamento</a>
+          </nav>
       </div>
     </>
   )
