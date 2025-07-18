27258f559eb07e1ef120de163d439e80b88f59bc
Louise Morais <louisemorais3@gmail.com>
2025-07-18 17:26:52 -0300

    init: inicializando repositorio e subindo o header

---
+++ src/App.jsx
@@ -0,0 +1,17 @@
+import './App.css'
+
+function App() {
+
+  return (
+    <>
+      <div>
+        <header className='cabecalho'>
+          <h1>Energia Solar 365</h1>
+          <p>Economize com o sol. Invista no seu futuro!</p>
+        </header>
+      </div>
+    </>
+  )
+}
+
+export default App
